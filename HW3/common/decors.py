7375cec6e84a7ade55d51ae3b21e2f6e89702248
VikulovM5 <vikulov.1991@mail.ru>
2022-01-27 16:53:56 +0300

    HW6

--- HW3/common/decors.py
+++ HW3/common/decors.py
@@ -1,9 +1,10 @@
+import socket
 import sys
 import logging
 from functools import wraps
-
-import HW3.log.client_log_config
-import HW3.log.server_log_config
+sys.path.append('../')
+import log.client_log_config
+import log.server_log_config
 
 
 if sys.argv[0].find('client') == -1:
@@ -22,3 +23,24 @@ class Log:
             )
             return res
         return log_saver()
+
+
+def login_required(func):
+    def checker(*args, **kwargs):
+        from server.core import MessageProcessor
+        from common.variables import ACTION, PRESENCE
+        if isinstance(args[0], MessageProcessor):
+            found = False
+            for arg in args:
+                if isinstance(arg, socket.socket):
+                    for client in args[0].names:
+                        if args[0].names[client] == arg:
+                            found = True
+            for arg in args:
+                if isinstance(arg, dict):
+                    if ACTION in arg and arg[ACTION] == PRESENCE:
+                        found = True
+            if not found:
+                raise TypeError
+        return func(*args, **kwargs)
+    return checker
--- HW3/launcher.py
+++ HW3/launcher.py
@@ -1,24 +1,31 @@
 import subprocess
 
-process = []
 
-while True:
-    ACTION = input('Выберите действие: q - выход, s - запуск сервера и клиентов, x - закрыть все окна: ')
+def main():
+    process = []
 
-    if ACTION == 'q':
-        break
-    elif ACTION == 's':
-        process.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
-        try:
-            i = 0
-            cl_number = int(input('Введите количество запускаемых клиентов'))
-            if i < cl_number:
-                param = f'python client.py -n Client{i+1}'
-                process.append(subprocess.Popen(param, creationflags=subprocess.CREATE_NEW_CONSOLE))
-                i += 1
-        except ValueError:
-            print('Необходимо ввести число')
+    while True:
+        ACTION = input('Выберите действие: q - выход, s - запуск сервера и клиентов, x - закрыть все окна: ')
 
-    if ACTION == 'x':
-        while process:
-            process.pop().kill()
+        if ACTION == 'q':
+            break
+        elif ACTION == 's':
+            process.append(subprocess.Popen('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE))
+        elif ACTION == 'k':
+            print('Убедитесь, что на сервере зарегистрировано необходимо количество клиентов с паролем 123456')
+            print('Первый запуск может быть достаточно долгим из-за генерации ключей')
+            clients_count = int(
+                input('Введите количество тестовых клиентов для запуска: '))
+            # Запускаем клиентов:
+            for i in range(clients_count):
+                process.append(
+                    subprocess.Popen(
+                        f'python client.py -n test{i + 1} -p 123456',
+                        creationflags=subprocess.CREATE_NEW_CONSOLE))
+        elif ACTION == 'x':
+            while process:
+                process.pop().kill()
+
+
+if __name__ == '__main__':
+    main()
